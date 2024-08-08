321ac7a5ffe0e06ce00d8849e0f5898ea15366ba
kratika-singh <105146516+kratika-singh@users.noreply.github.com>
2024-08-08 20:26:18 +0530

    bcrypt issue

--- helper/authHelper.js
+++ helper/authHelper.js
@@ -1,10 +1,10 @@
-import bcrypt from'bcrypt';
+import bcryptjs from'bcryptjs';
 // password hashing
 
 export const hashPassword = async(password) =>{
     try{
         const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password,saltRounds);
+        const hashedPassword = await bcryptjs.hash(password,saltRounds);
         return hashedPassword;
     }catch(error){
         console.log(error);
@@ -12,5 +12,5 @@ export const hashPassword = async(password) =>{
 };
 
 export const comparePassword = async(password,hashedPassword)=>{
-    return bcrypt.compare(password,hashedPassword);
+    return bcryptjs.compare(password,hashedPassword);
 };
